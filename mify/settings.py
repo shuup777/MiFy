3417a6c69c1998676e221ea54e384ba8513fc88e
shuup777 <pashuuphehey@gmail.com>
2025-12-12 06:24:23 +0700

    Update settings.py

--- mify/settings.py
+++ mify/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=6od5dns1rlrnz85tiv@&vp$2uqeryw3efho@b!=j(#+lb5-f(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
